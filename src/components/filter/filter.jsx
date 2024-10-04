8832c23cc00a20290ac37b671d1c8cee771b8027
MahamudM90 <mahamud15-10467@diu.edu.bd>
2024-10-04 20:13:43 +0600

    create fake productsdata

---
+++ src/components/filter/filter.jsx
@@ -0,0 +1,15 @@
+export default function Filter() {
+
+    const productsdata = [
+        { name: 'Smart-Watch', price: '$200', category: 'computing'},
+        { name: 'Laptop', price: '$600', category: 'computing' },
+        { name: 'Polo-Shirt', price: '$20', category: 'cloth' },
+        { name: 'Jeans-Pant', price: '$10', category: 'cloth' }
+    ]
+
+    return (
+        <>
+
+        </>
+    )
+}
