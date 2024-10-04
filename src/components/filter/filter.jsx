b56ce747098738c2de66452ad585560e3c05ee85
MahamudM90 <mahamud15-10467@diu.edu.bd>
2024-10-04 20:41:34 +0600

    react hooks && condition done for filter selected product

--- src/components/filter/filter.jsx
+++ src/components/filter/filter.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react"
+
 export default function Filter() {
 
     const productsdata = [
-        { name: 'Smart-Watch', price: '$200', category: 'computing'},
-        { name: 'Laptop', price: '$600', category: 'computing' },
-        { name: 'Polo-Shirt', price: '$20', category: 'cloth' },
-        { name: 'Jeans-Pant', price: '$10', category: 'cloth' }
+        { name: 'Smart-Watch', price: '300', category: 'computing' },
+        { name: 'Laptop', price: '610', category: 'computing' },
+        { name: 'Polo-Shirt', price: '20', category: 'cloth' },
+        { name: 'Jeans-Pant', price: '10', category: 'cloth' },
+        { name: 'Mobile', price: '40', category: 'computing' }
     ]
+
+    const [category, setCategory] = useState('All')
+    const [price, setPrice] = useState('All')
+
+    //Filter selected product 
+    const filterProduct = productsdata.filter(product => {
+        const categoryMatch = category = 'All' || product.category === category
+        const priceMatch = price = 'All' || (price == 'low' && product.price < 30) || (price === 'moderate' && product.price <= 300) || (price === 'higher' && product.price > 600);
+        return categoryMatch && priceMatch;
+    })
 
     return (
         <>
